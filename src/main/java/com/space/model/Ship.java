677a8ade687179d583ea0bce071599db174d1615
natalyBar <nataly.v.baranova@gmail.com>
2019-09-12 00:24:53 +0300

    First Commit. Good luck with this Project!

---
+++ src/main/java/com/space/model/Ship.java
@@ -0,0 +1,15 @@
+package com.space.model;
+
+import java.util.Date;
+
+public class Ship {
+    private long id;
+    private String name;
+    private String planet;
+    private ShipType shipType;
+    private Date prodDate;
+    private Boolean isUsed;
+    private Double speed;
+    private Integer crewSize;
+    private Double rating;
+}
